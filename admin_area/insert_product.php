df0ca5297ac59429de0bb1f5b818d10b6310fede
Matheus Cardoso da Silva Antonio <75143307+Cardoso05@users.noreply.github.com>
2021-09-08 22:19:51 -0300

    part 1/2 insert
    backend

--- admin_area/insert_product.php
+++ admin_area/insert_product.php
@@ -281,3 +281,30 @@ include("includes/db.php");
 </body>
 
 </html>
+
+<?php
+
+if (isset($_POST['submit'])) {
+
+    $product_title = $_POST['product_title'];
+    $product_cat = $_POST['product_cat'];
+    $cat = $_POST['cat'];
+    $product_price = $_POST['product_price'];
+    $product_keywords = $_POST['product_keywords'];
+    $product_desc = $_POST['product_desc'];
+
+    $product_img1 = $_FILES['product_img1']['name'];
+    $product_img2 = $_FILES['product_img2']['name'];
+    $product_img3 = $_FILES['product_img3']['name'];
+
+    $temp_name1 = $_FILES['product_img1']['tmp_name'];
+    $temp_name2 = $_FILES['product_img2']['tmp_name'];
+    $temp_name3 = $_FILES['product_img3']['tmp_name'];
+
+    move_uploaded_file($temp_name1, "product_images/$product_img1");
+    move_uploaded_file($temp_name2, "product_images/$product_img2");
+    move_uploaded_file($temp_name3, "product_images/$product_img3");
+}
+
+
+?>
