fc45c5d90b022bc32ee8eed57bbca745d847dd20
Matheus Cardoso da Silva Antonio <75143307+Cardoso05@users.noreply.github.com>
2021-10-05 23:09:27 -0300

    Admin Panel Creating
    Dashboard 1/2

---
+++ admin_area/dashboard.php
@@ -0,0 +1,264 @@
+<div class="row">
+    <!-- row begin-->
+    <div class="col-lg-12">
+        <!-- col-lg-12 begin-->
+        <h1 class="page-header"> Dashboard </h1>
+
+        <ol class="breadcrumb">
+            <!-- breadcrumb begin-->
+
+
+            <li class="active">
+                <!-- active begin-->
+
+                <i class="fa fa-dashboard"></i> Dashboard
+
+            </li><!-- active finish -->
+
+        </ol><!-- breadcrumb finish-->
+
+    </div><!-- row finish-->
+
+</div><!-- col-lg-12 finish-->
+
+<div class="row">
+    <!-- row begin-->
+
+    <div class="col-lg-3 col-md-6">
+        <!-- col-lg-3 col-md-6 begin -->
+
+        <div class="panel panel-primary">
+            <!-- panel panel-primary begin-->
+
+            <div class="panel-heading">
+                <!-- panel-heading begin-->
+
+                <div class="row">
+                    <!-- row begin-->
+
+                    <div class="col-xs-3">
+                        <!-- col-xs-3 begin-->
+
+                        <i class="fa fa-tasks fa-5x"></i>
+
+                    </div> <!-- col-xs-3 finish-->
+
+                    <div class="col-xs-9 text-right">
+                        <!-- col-xs-9 text-right begin-->
+
+                        <div class="huge">17</div>
+
+                        <div>Products</div>
+
+
+                    </div><!-- col-xs-9 text-right finish-->
+
+                </div> <!-- row finish-->
+
+            </div> <!-- panel-heading finish-->
+
+            <a href="index.php?view_produts.php">
+                <!-- a href begin -->
+
+                <div class="panel-footer">
+                    <!-- a panel-footer begin -->
+
+                    <span class="pull-left">
+                        <!-- pull-left begin-->
+                        View Details
+                    </span> <!-- pull-left finish-->
+
+                    <span class="pull-right">
+                        <!-- pull-right begin-->
+                        <i class="fa fa-arrow-circle-right"></i>
+                    </span> <!-- pull-right finish-->
+
+                    <div class="clearfix"></div>
+
+                </div><!-- panel-footer finish -->
+
+            </a><!-- a href finish -->
+
+        </div><!-- panel panel-primary finish-->
+
+    </div><!-- col-lg-3 col-md-6 finish-->
+
+
+    <div class="col-lg-3 col-md-6">
+        <!-- col-lg-3 col-md-6 begin -->
+
+        <div class="panel panel-green">
+            <!-- panel panel-green begin-->
+
+            <div class="panel-heading">
+                <!-- panel-heading begin-->
+
+                <div class="row">
+                    <!-- row begin-->
+
+                    <div class="col-xs-3">
+                        <!-- col-xs-3 begin-->
+
+                        <i class="fa fa-users fa-5x"></i>
+
+                    </div> <!-- col-xs-3 finish-->
+
+                    <div class="col-xs-9 text-right">
+                        <!-- col-xs-9 text-right begin-->
+
+                        <div class="huge">17</div>
+
+                        <div>Customers</div>
+
+
+                    </div><!-- col-xs-9 text-right finish-->
+
+                </div> <!-- row finish-->
+
+            </div> <!-- panel-heading finish-->
+
+            <a href="index.php?view_customers.php">
+                <!-- a href begin -->
+
+                <div class="panel-footer">
+                    <!-- a panel-footer begin -->
+
+                    <span class="pull-left">
+                        <!-- pull-left begin-->
+                        View Details
+                    </span> <!-- pull-left finish-->
+
+                    <span class="pull-right">
+                        <!-- pull-right begin-->
+                        <i class="fa fa-arrow-circle-right"></i>
+                    </span> <!-- pull-right finish-->
+
+                    <div class="clearfix"></div>
+
+                </div><!-- panel-footer finish -->
+
+            </a><!-- a href finish -->
+
+        </div><!-- panel panel-green finish-->
+
+    </div><!-- col-lg-3 col-md-6 finish-->
+
+    <div class="col-lg-3 col-md-6">
+        <!-- col-lg-3 col-md-6 begin -->
+
+        <div class="panel panel-orange">
+            <!-- panel panel-orange begin-->
+
+            <div class="panel-heading">
+                <!-- panel-heading begin-->
+
+                <div class="row">
+                    <!-- row begin-->
+
+                    <div class="col-xs-3">
+                        <!-- col-xs-3 begin-->
+
+                        <i class="fa fa-tags fa-5x"></i>
+
+                    </div> <!-- col-xs-3 finish-->
+
+                    <div class="col-xs-9 text-right">
+                        <!-- col-xs-9 text-right begin-->
+
+                        <div class="huge">5</div>
+
+                        <div>Product Categories</div>
+
+
+                    </div><!-- col-xs-9 text-right finish-->
+
+                </div> <!-- row finish-->
+
+            </div> <!-- panel-heading finish-->
+
+            <a href="index.php?view_p_cats.php">
+                <!-- a href begin -->
+
+                <div class="panel-footer">
+                    <!-- a panel-footer begin -->
+
+                    <span class="pull-left">
+                        <!-- pull-left begin-->
+                        View Details
+                    </span> <!-- pull-left finish-->
+
+                    <span class="pull-right">
+                        <!-- pull-right begin-->
+                        <i class="fa fa-arrow-circle-right"></i>
+                    </span> <!-- pull-right finish-->
+
+                    <div class="clearfix"></div>
+
+                </div><!-- panel-footer finish -->
+
+            </a><!-- a href finish -->
+
+        </div><!-- panel panel-orange finish-->
+
+    </div><!-- col-lg-3 col-md-6 finish-->
+
+    <div class="col-lg-3 col-md-6">
+        <!-- col-lg-3 col-md-6 begin -->
+
+        <div class="panel panel-red">
+            <!-- panel panel-red begin-->
+
+            <div class="panel-heading">
+                <!-- panel-heading begin-->
+
+                <div class="row">
+                    <!-- row begin-->
+
+                    <div class="col-xs-3">
+                        <!-- col-xs-3 begin-->
+
+                        <i class="fa fa-shopping-cart fa-5x"></i>
+
+                    </div> <!-- col-xs-3 finish-->
+
+                    <div class="col-xs-9 text-right">
+                        <!-- col-xs-9 text-right begin-->
+
+                        <div class="huge">25</div>
+
+                        <div>Orders</div>
+
+
+                    </div><!-- col-xs-9 text-right finish-->
+
+                </div> <!-- row finish-->
+
+            </div> <!-- panel-heading finish-->
+
+            <a href="index.php?view_p_cats.php">
+                <!-- a href begin -->
+
+                <div class="panel-footer">
+                    <!-- a panel-footer begin -->
+
+                    <span class="pull-left">
+                        <!-- pull-left begin-->
+                        View Details
+                    </span> <!-- pull-left finish-->
+
+                    <span class="pull-right">
+                        <!-- pull-right begin-->
+                        <i class="fa fa-arrow-circle-right"></i>
+                    </span> <!-- pull-right finish-->
+
+                    <div class="clearfix"></div>
+
+                </div><!-- panel-footer finish -->
+
+            </a><!-- a href finish -->
+
+        </div><!-- panel panel-red finish-->
+
+    </div><!-- col-lg-3 col-md-6 finish-->
+
+</div><!-- row finish-->
--- admin_area/index.php
+++ admin_area/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -8,22 +9,35 @@
     <link rel="stylesheet" href="font-awsome/css/font-awesome.min.css">
     <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
 
-    <div id="wrapper"><!-- #wrapper begin -->
-       
-       <?php include("includes/sidebar.php"); ?>
-       
-        <div id="page-wrapper"><!-- #page-wrapper begin -->
-            <div class="container-fluid"><!-- container-fluid begin -->
-                
-                
-                
+    <div id="wrapper">
+        <!-- #wrapper begin -->
+
+        <?php include("includes/sidebar.php"); ?>
+
+        <div id="page-wrapper">
+            <!-- #page-wrapper begin -->
+            <div class="container-fluid">
+                <!-- container-fluid begin -->
+
+                <?php
+
+                if (isset($_GET['dashboard'])) {
+
+                    include("dashboard.php");
+                }
+
+
+                ?>
+
             </div><!-- container-fluid finish -->
         </div><!-- #page-wrapper finish -->
     </div><!-- wrapper finish -->
 
-<script src="js/jquery-331.min.js"></script>     
-<script src="js/bootstrap-337.min.js"></script>           
+    <script src="js/jquery-331.min.js"></script>
+    <script src="js/bootstrap-337.min.js"></script>
 </body>
+
 </html>
