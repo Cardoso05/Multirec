7517a184f29c5eee54728ba3d7c16089e9bded2a
Matheus Cardoso da Silva Antonio <75143307+Cardoso05@users.noreply.github.com>
2021-10-06 21:17:18 -0300

    Admin Panel Creating
    Dashboard 2/2

--- admin_area/dashboard.php
+++ admin_area/dashboard.php
@@ -1,5 +1,5 @@
 <div class="row">
-    <!-- row begin-->
+    <!-- row-1 begin-->
     <div class="col-lg-12">
         <!-- col-lg-12 begin-->
         <h1 class="page-header"> Dashboard </h1>
@@ -17,12 +17,12 @@
 
         </ol><!-- breadcrumb finish-->
 
-    </div><!-- row finish-->
+    </div><!-- col-lg-12 finish-->
 
-</div><!-- col-lg-12 finish-->
+</div><!-- row-1 finish-->
 
 <div class="row">
-    <!-- row begin-->
+    <!-- row-1 begin-->
 
     <div class="col-lg-3 col-md-6">
         <!-- col-lg-3 col-md-6 begin -->
@@ -261,4 +261,178 @@
 
     </div><!-- col-lg-3 col-md-6 finish-->
 
-</div><!-- row finish-->
+</div><!-- row-2 finish-->
+
+<div class="row">
+    <!-- row-3 begin-->
+    <div class="col-lg-8">
+        <!-- col-lg-8 begin-->
+        <div class="panel panel-primary">
+            <!-- panel panel-primary begin-->
+            <div class="panel-heading">
+                <!-- panel-heading begin-->
+                <h3 class="panel-title">
+                    <i class="fa fa-money fa-fw"></i> New Orders
+                </h3>
+            </div><!-- panel-heading finish-->
+
+            <div class="panel-body">
+                <!-- panel-body begin-->
+
+                <div class="table-responsive">
+                    <!-- table-responsive begin-->
+
+                    <table class="table table-hover table-striped table-bordered">
+                        <!-- table table-hover table-striped table-bordered begin-->
+
+                        <thead>
+                            <!-- thead begin-->
+
+                            <tr>
+                                <!-- th begin-->
+
+                                <th> Order no: </th>
+                                <th> Customer Email: </th>
+                                <th> Invoice No: </th>
+                                <th> Product ID: </th>
+                                <th> Product Qty: </th>
+                                <th> Product Size: </th>
+                                <th> Status: </th>
+
+                            </tr><!-- th finish-->
+
+                        </thead><!-- thead finish-->
+
+                        <tbody>
+                            <!-- tbody begin-->
+
+                            <tr>
+                                <!-- tr begin-->
+
+                                <td> 1 </td>
+                                <td> zekkacardoso18@gmail.com </td>
+                                <td> 45545 </td>
+                                <td> 4 </td>
+                                <td> 2 </td>
+                                <td> large </td>
+                                <td> Pending </td>
+
+                            </tr><!-- tr finish-->
+
+                            <tr>
+                                <!-- tr begin-->
+
+                                <td> 1 </td>
+                                <td> zekkacardoso18@gmail.com </td>
+                                <td> 45545 </td>
+                                <td> 4 </td>
+                                <td> 2 </td>
+                                <td> large </td>
+                                <td> Pending </td>
+
+                            </tr><!-- tr finish-->
+
+                            <tr>
+                                <!-- tr begin-->
+
+                                <td> 1 </td>
+                                <td> zekkacardoso18@gmail.com </td>
+                                <td> 45545 </td>
+                                <td> 4 </td>
+                                <td> 2 </td>
+                                <td> large </td>
+                                <td> Pending </td>
+
+                            </tr><!-- tr finish-->
+
+                            <tr>
+                                <!-- tr begin-->
+
+                                <td> 1 </td>
+                                <td> zekkacardoso18@gmail.com </td>
+                                <td> 45545 </td>
+                                <td> 4 </td>
+                                <td> 2 </td>
+                                <td> large </td>
+                                <td> Pending </td>
+
+                            </tr><!-- tr finish-->
+
+                        </tbody><!-- tbody finish-->
+
+                    </table><!-- table table-hover table-striped table-bordered finish-->
+
+                </div><!-- table-responsive finish-->
+
+                <div class="text-right">
+                    <!-- text-right begin-->
+
+                    <a href="index.php?view_orders">
+                        <!-- a href finish-->
+
+                        View All Orders <i class="fa fa-arrow-circle-right"></i>
+
+                    </a><!-- a href begin-->
+
+                </div><!-- text-right finish-->
+
+            </div><!-- panel-body finish-->
+
+        </div><!-- panel panel-primary finish-->
+
+    </div><!-- col-lg-8 finish-->
+
+    <div class="col-md-4">
+        <!-- col-md-4 begin-->
+
+        <div class="panel">
+            <!-- panel beign-->
+
+            <div class="panel-body">
+                <!-- panel-body begin-->
+
+                <div class="md-md thumb-info">
+                    <!-- md-md thumb-info begin-->
+
+                    <img src="admin_images/erika.jpg" alt="" class="img-responsive">
+
+                    <div class="thumb-info-title">
+                        <!-- thumb-info-title begin-->
+
+                        <span class="thumb-info-inner">MrGhie</span>
+                        <span class="thumb-info-type"> Web Developer</span>
+
+                    </div><!-- thumb-info-title finish-->
+
+                </div><!-- md-md thumb-info finish-->
+
+                <div class="md-md">
+                    <!-- md-md beign-->
+
+                    <div class="widget-content-expanded">
+                        <!-- widget-content-expanded beign-->
+
+                        <i class="fa fa-user"></i> <span> Email: </span> zekkacardoso18@gmail.com<br>
+                        <i class="fa fa-flag"></i> <span> Country: </span> Brasil<br>
+                        <i class="fa fa-envelope"></i> <span> Contact: </span> (11) 98600-5144<br>
+
+                    </div><!-- widget-content-expanded finish-->
+
+                    <hr class="dotted short " style="margin-top: 10px 0 10px 0;">
+
+                    <h5 class="text-muted"> About Me </h5>
+
+                    <p> This aplication is created by medv media,if you willing to contact me, please click this link <a
+                            href="#">M-Dev-Media</a><br>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Molestias architecto aliquam doloremque! Temporibus, soluta atque? Ipsum labore maiores odit
+                        est! Omnis atque vero nobis eum quam dicta sit vel quos?</p>
+
+                </div><!-- md-md finish-->
+
+            </div><!-- panel-body finish-->
+
+        </div><!-- panel finish-->
+
+    </div><!-- col-md-4 finish-->
+
+</div><!-- row-3 finish-->
